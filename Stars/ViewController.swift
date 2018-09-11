81754859642827a956b1890ebe23de61be53d892
Jonah Bergevin <jonahbergevin@gmail.com>
2018-09-11 10:15:25 -0600

    first commit

---
+++ Stars/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stars
+//
+//  Created by Madison Waters on 9/11/18.
+//  Copyright © 2018 Jonah Bergevin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
